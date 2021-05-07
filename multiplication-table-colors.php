c6b8091e794aae2df60e4bcae0932d42b0edab62
unknown <Shevchenko.oleg.1992@gmail.com>
2021-05-07 16:10:15 +0300

    multiplication-table-colors

--- multiplication-table-colors.php
+++ multiplication-table-colors.php
@@ -12,7 +12,6 @@
     <?php
         $mass = [1,2,3,4,5,6,7,8,9,10];
         
-
         function multiply($multiplier){ ?>
             <div class="multiplier-wrapper">
             <?php
@@ -20,8 +19,68 @@
                     <div class="multiplier-item">
                     <?php
                         for ($i = 1; $i <= count($multiplier); $i++){
+
                             $result = $value * $i;
-                            echo "$value  x  $i = $result <br>";
+
+                            $value2 = $value; 
+
+                            $i_2 = $i;
+
+                            $value2 = str_split($value2);
+                            foreach ($value2 as $value_result2) {
+                                if($value_result2 == 1){
+                                    $color2 = 'red';
+                                }elseif($value_result2 == 2){
+                                    $color2 = 'green';
+                                }elseif($value_result2 == 3){
+                                    $color2 = 'yellow';
+                                }elseif($value_result2 == 4){
+                                    $color2 = 'blue';
+                                }else{
+                                    $color2 = '';
+                                }
+                                echo "<span class='$color2'>$value_result2</span>";
+                            }
+
+                            echo " x ";
+
+                            $i_2 = str_split($i_2);
+                            foreach ($i_2 as $i_2_result) {
+                                if($i_2_result == 1){
+                                    $color = 'red';
+                                }elseif($i_2_result == 2){
+                                    $color = 'green';
+                                }elseif($i_2_result == 3){
+                                    $color = 'yellow';
+                                }elseif($i_2_result == 4){
+                                    $color = 'blue';
+                                }else{
+                                    $color = '';
+                                }
+                                echo "<span class='$color'>$i_2_result</span>";
+                            }
+
+                            echo " = ";
+
+                            $result = str_split($result);
+                            foreach ($result as $value_result) {
+                                if($value_result == 1){
+                                    $color3 = 'red';
+                                }elseif($value_result== 2){
+                                    $color3 = 'green';
+                                }elseif($value_result == 3){
+                                    $color3 = 'yellow';
+                                }elseif($value_result == 4){
+                                    $color3 = 'blue';
+                                }else{
+                                    $color3 = '';
+                                }
+                                echo  "<span class='$color3'>$value_result</span>";
+                                
+                            }
+
+                            echo "<br>";
+                            
                         }
                     ?>
                     </div>
@@ -41,6 +100,18 @@
             margin: 2px;
             border:  1px solid #000;
             padding: 10px;
+        }
+        .red{
+            color: red;
+        }
+        .green{
+            color: green;
+        }
+        .yellow{
+            color: yellow;
+        }
+        .blue{
+            color: blue;
         }
     </style>
 
